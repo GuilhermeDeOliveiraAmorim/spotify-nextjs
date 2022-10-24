87e65b92869013a6d197a25a7ddc431b7687d542
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-10-24 19:07:23 -0300

    feat: awesome

---
+++ pages/week/index.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import React from "react";
+import { Footer } from "../../components/footer";
+import { Header } from "../../components/header";
+import { Page } from "../../components/page";
+import { IWeek } from "./IWeek";
+
+const Week: NextPage = (props: IWeek) => {
+    const { } = props;
+
+    return (
+        <div className="flex flex-col bg-spotify-green overflow-auto">
+            <Header />
+            <main className="gap-2 p-2">
+                <Page pageTitle="Week" />
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Week;
