a6ad96e4c01b17589b0978fa2065be03b691cd7c
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-10-30 13:18:23 -0300

    feat: autorization ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,25 @@
-import axios from 'axios';
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import Footer from '../components/footer';
 import Header from '../components/header';
-import Ranking from '../components/ranking';
 
 const Home: NextPage = () => {
-	const OAUTH_TOKEN = "BQDvaC3HLcoiGu5BjIAbqpPt_NGaT2fzGvImhxC9CgyNzTwm4N1jdisVPnuNoBPLJrllKlZ_AcY6mwBg6JxpivNXNOQx-F-ztBv0p13leQx34v8tWgTso2TEKfS2vrqYjnYlWWIhTOPWcCNSqEWtgcp2wVslIqn1mpOa8PHeoD50dlzECArX";
 
-	const [tracks, setTracks] = useState([]);
+	const client_id = "83dc318f5c3c4ca0b4bfb6c7f7333631";
+	const response_type = 'code';
+	const scope = "user-top-read";
+	const redirect_uri = "http://localhost:3000/home";
+	const uri_authorize = `https://accounts.spotify.com/authorize?response_type=${response_type}&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}`;
 
-	async function getTopTracks() {
-
-		const data = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=5&offset=5", {
-			headers: {
-				Authorization: `Bearer ${OAUTH_TOKEN}`
-			},
-		});
-
-		setTracks(data.data.items);
-	};
-
-	useEffect(() => {
-		getTopTracks();
-	}, [])
-
-
-	console.log(tracks);
+	function login() {
+		location.href = uri_authorize;
+	}
 
 	return (
 		<div className="flex flex-col h-screen justify-between bg-spotify-green overflow-auto">
 			<Header />
 			<main className="flex flex-col sm:flex-row lg:flex-row gap-2 p-2">
-				<Ranking
-					period="day"
-					title="Day Ranking"
-					urlImg="https://mosaic.scdn.co/300/ab67616d0000b27338e1ac1f00e0eada50b0e5b6ab67616d0000b2735512578259c721cb16808461ab67616d0000b273b944f430d0489ae7c6a1fd02ab67616d0000b273d46dec4689741b1400c3df95"
-				/>
+				<button onClick={() => login()}>Entrar</button>
 			</main>
 			<Footer />
 		</div>
---
+++ spotify/spotify.ts
@@ -0,0 +1,35 @@
+import querystring from "querystring";
+
+const client_id = "83dc318f5c3c4ca0b4bfb6c7f7333631";
+const client_secret = "ff5a6136396649419f1af87d08954e43";
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+const getAccessToken = async () => {
+    const response = await fetch(TOKEN_ENDPOINT, {
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${basic}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: querystring.stringify({
+            grant_type: "refresh_token",
+            refresh_token,
+        }),
+    });
+
+    return response.json();
+};
+
+export const getNowPlaying = async () => {
+    const { access_token } = await getAccessToken();
+
+    return fetch(NOW_PLAYING_ENDPOINT, {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+};
